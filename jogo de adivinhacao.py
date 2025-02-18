02de52ca3164503ba46aa510fbe39a5a899d8f64
GeovanaVarela <geovana.terres@escola.pr.gov.br>
2025-02-18 08:41:25 -0300

    jogo

---
+++ jogo de adivinhacao.py
@@ -0,0 +1,17 @@
+print("**********")
+print("Bem vindo ao jogo de adivinhação!")
+print("**********")
+
+numero_secreto=24
+
+chute_str = input ("Digite o seu número:")
+print("Voce digitou:", chute_str)
+chute = int (chute_str)
+
+if (numero_secreto == chute):
+    print("Voce acertou!")
+else
+    print("Voce errou!")
+
+    print("Fim de jogo!")
+
